a6d4082de9fcb0aff406646990acfe2a00f1b69a
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-02-06 13:25:36 -0300

    Commit inicial

---
+++ DictionMaster/DictionMaster/DictionMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionMasterApp.swift
+//  DictionMaster
+//
+//  Created by Rodrigo Ferreira Pereira on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DictionMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
